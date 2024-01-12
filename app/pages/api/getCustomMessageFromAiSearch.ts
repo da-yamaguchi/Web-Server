60bdd94f87d32f5ad49562b1892038e0ee646403
LAPTOP-5TUIN3RB\Z0192 <d-yamaguchi@link-tech.jp>
2024-01-12 22:55:40 +0900

    AI Searchとの連携作成。 応答は取得できたが、無関係な質問にたいして関連性の低い検索結果が取得できてしまうため、調整が必要 score、Reranker Scoreか？

--- app/pages/api/getCustomMessageFromAiSearch.ts
+++ app/pages/api/getCustomMessageFromAiSearch.ts
@@ -2,16 +2,15 @@ import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { customLog } from '@/utils/customLog';
 import { AIRequest } from '@/utils/chat_gpt/ai_requset';
-// import { SearchClient, AzureKeyCredential } from '@azure/search-documents';
-import { SearchClient, AzureKeyCredential as SearchKeyCredential, SearchFieldArray,} from '@azure/search-documents';
+import { SearchClient, AzureKeyCredential as SearchKeyCredential, SearchDocumentsResult } from '@azure/search-documents';
   
 const API_KEY: string = process.env.AZURE_AI_API_KEY_CHATGPT35!;
 const EMBEDDING_DEPLOYNAME: string = process.env.OPENAI_API_EMBEDDING_DEPLOYNAME!;
 const OPENAI_URL: string = process.env.OPENAI_API_URL!;
 
-const searchendpoint = process.env.SEARCH_API_ENDPOINT;
-const searchapiKey = process.env.SEARCH_API_KEY;
-const searchindexname = process.env.SEARCH_INDEXNAME;
+const searchendpoint: string = process.env.SEARCH_API_ENDPOINT!;
+const searchapiKey: string = process.env.SEARCH_API_KEY!;
+const searchindexname: string = process.env.SEARCH_INDEXNAME!;
 
 const searchClient = new SearchClient(
     searchendpoint,
@@ -20,115 +19,85 @@ const searchClient = new SearchClient(
 );
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'POST') {
-        customLog(req.body,"DEBUG");
-        const requestMessage: string = req.body.message;
-    
-        if(requestMessage == "") {
-          customLog("requestMessage is empty","DEBUG");
-          return res.status(200).json({ success: false, message: "request message is empty" });
-        }
+  if (req.method === 'POST') {
+    customLog(req.body,"DEBUG");
+    const requestMessage: string = req.body.message;
 
-        // generateEmbeddings呼び出し時に型変換する
-        const vector: number[] = await generateEmbeddings(requestMessage) as number[];
-        
-        const response = await searchClient.search(requestMessage, {
-//          vectorQueries: [
-          //   {
-          //     kind: 'vector',
-          //     vector: await generateEmbeddings(requestMessage),
-          //     kNearestNeighborsCount: 3,
-          //     fields: ['vector'],
-          //   },
-          // ],
-          vectorSearchOptions: {
-            queries:  [{
-              kind: 'vector',
-              //vector: await generateEmbeddings(requestMessage),
-              vector: vector,
-              kNearestNeighborsCount: 3,
-              fields: ['vector'],
-            },
-          ],
-          },
-          select: ['chunk', 'title'],
-          queryType: 'semantic',
-          top: 1,
-          semanticSearchOptions: {
-            answers: {
-              answerType: 'extractive',
-              count: 3,
-            },
-            captions: {
-              captionType: 'extractive',
-              //count: 3,
-            },
-            configurationName: 'vector-kitei-semantic-configuration',
-          },
-        });
-              
-        // const response = await searchClient.search(requestMessage, {
-        //   vectorQueries: [
-        //       {
-        //         kind: 'vector',
-        //         vector: await generateEmbeddings(requestMessage),
-        //         kNearestNeighborsCount: 3,
-        //         fields: ['vector'],
-        //       },
-        //     ],
-        //     select: ['chunk', 'title'],
-        //     queryType: 'semantic',
-        //     top: 1,
-        //     semanticSearchOptions: {
-        //       answers: {
-        //         answerType: 'extractive',
-        //         count: 3,
-        //       },
-        //       captions: {
-        //         captionType: 'extractive',
-        //         //count: 3,
-        //       },
-        //       configurationName: 'vector-kitei-semantic-configuration',
-        //     },
-        //   });
-          //context.log(`\nSemantic Hybrid search results:`);
-          customLog("Semantic Hybrid search results:","DEBUG");
-        
-          let message = '';
-          let title = '';
-        
-          customLog(response.results,"DEBUG");
-          for await (const result of response.results) {
-            //context.log(`Title: ${result.document.title}`);
-            //title = result.document.title;
-            //context.log(`Reranker Score: ${result.rerankerScore}`); // Reranker score is the semantic score
-        
-            if (result.captions) {
-              const caption = result.captions[0];
-              if (caption.highlights) {
-                //context.log(`Caption: ${caption.highlights}`);
-              } else {
-                //context.log(`Caption: ${caption.text}`); // 検索ワードに一番近い抜粋
-                //message = caption.text;
-              }
-            }
-            //context.log(`\n`);
-          }
-        
-          if (message.trim() === '') {
-            //console.log('AI Search Message is empty');
-          } else {
-            message = message + '\n' + title;
-          }
-        
+    if(requestMessage == "") {
+      customLog("requestMessage is empty","DEBUG");
+      return res.status(200).json({ success: false, message: "request message is empty" });
     }
+
+    // generateEmbeddings呼び出し時に型変換する
+    const vector: number[] = await generateEmbeddings(requestMessage) as number[];
+    
+    const response = await searchClient.search(requestMessage, {
+      vectorSearchOptions: {
+        queries:  [{
+          kind: 'vector',
+          vector: vector,
+          kNearestNeighborsCount: 3,
+          fields: ['vector'],
+        },
+      ],
+      },
+      select: ['chunk', 'title'],
+      queryType: 'semantic',
+      top: 1,
+      semanticSearchOptions: {
+        answers: {
+          answerType: 'extractive',
+          count: 3,
+        },
+        captions: {
+          captionType: 'extractive',
+          //count: 3,
+        },
+        configurationName: 'vector-kitei-semantic-configuration',
+      },
+    });
+          
+    customLog("Semantic Hybrid search results:","DEBUG");
+  
+    let message = '';
+    let title = '';
+
+    for await (const result of response.results) {
+      //customLog(result,"DEBUG");
+      customLog("Title:" + result.document.title,"DEBUG");
+      title = result.document.title;
+      customLog("score:" + result.score,"DEBUG");
+      customLog("Reranker Score:" + result.rerankerScore,"DEBUG");
+  
+      if (result.captions) {
+        const caption = result.captions[0];
+        if (caption.highlights) {
+          customLog("Caption highlights:" + result.highlights,"DEBUG");
+        } else {
+          customLog("Caption text:" + caption.text,"DEBUG");
+          message = caption.text;
+        }
+      } else {
+        customLog("No result.captions","DEBUG");
+      }
+    }
+  
+    if (message.trim() === '') {
+      customLog("AI Search Message is empty","DEBUG");
+    } else {
+      message = message + '\n' + title;
+      customLog("Return Message:" + message,"DEBUG");
+      return res.status(200).json({ success: true, message: message });
+    }
+    return res.status(200).json({ success: false, message: "提供されているドキュメントに合致する検索結果はありません。" });
+  }
 };
 
 async function generateEmbeddings(text: string): Promise<unknown> {
   const apiVersion = '2023-09-01-preview';
 
   const response = await axios.post(
-    `${process.env.OPENAI_API_URL}${process.env.OPENAI_API_EMBEDDING_DEPLOYNAME}/embeddings?api-version=${apiVersion}`,
+    `${OPENAI_URL}${EMBEDDING_DEPLOYNAME}/embeddings?api-version=${apiVersion}`,
     {
       input: text,
       engine: 'text-embedding-ada-002',
@@ -136,106 +105,11 @@ async function generateEmbeddings(text: string): Promise<unknown> {
     {
       headers: {
         'Content-Type': 'application/json',
-        'api-key': process.env.OPENAI_API_KEY,
+        'api-key': API_KEY,
       },
     }
   );
   const embeddings = response.data.data[0].embedding;
+  //customLog("embeddings:" + embeddings,"DEBUG");
   return embeddings;
 }
-
-
-
-
-
-// function SemanticSearchComponent({ }) {
-//   const searchClient = new SearchClient(
-//       searchendpoint,
-//       searchindexname,
-//       new AzureKeyCredential(searchapiKey)
-//   );
-
-//   const [searchResults, setSearchResults] = useState<string>('');
-
-//   const doSemanticHybridSearch = async (query) => {
-//     try {
-//       const embeddings = await generateEmbeddings(query);
-
-//       const response = await searchClient.search(query, {
-//         vectorQueries: [{
-//           kind: "vector",
-//           vector: embeddings,
-//           kNearestNeighborsCount: 3,
-//           fields: ["vector"],
-//         }],
-//         select: ["chunk", "title"],
-//         queryType: "semantic",
-//         top: 1,
-//         semanticSearchOptions: {
-//           answers: {
-//             answerType: "extractive",
-//             count: 3
-//           },
-//           captions: {
-//             captionType: "extractive",
-//             count: 3
-//           },
-//           configurationName: "vector-kitei-semantic-configuration",
-//         }
-//       });
-
-//       let message = "";
-//       let title = "";
-
-//       for await (const result of response.results) {
-//         title = result.document.title;
-
-//         if (result.captions) {
-//           const caption = result.captions[0];
-//           if (caption.highlights) {
-//             message += `Caption: ${caption.highlights}\n`;
-//           } else {
-//             message += `Caption: ${caption.text}\n`;  // 検索ワードに一番近い抜粋
-//           }
-//         }
-//       }
-
-//       if (message.trim() === '') {
-//         console.log('AI Search Message is empty');
-//       } else {
-//         message = message + "\n" + title;
-//         setSearchResults(message);
-//       }
-//     } catch (error) {
-//       console.error('Error during semantic search:', error);
-//     }
-//   };
-
-//   const generateEmbeddings = async (text) => {
-//     const apiVersion = "2023-09-01-preview";
-
-//     try {
-//       const response = await axios.post(
-//         `${OPENAI_URL}${EMBEDDING_DEPLOYNAME}/embeddings?api-version=${apiVersion}`,
-//         {
-//           input: text,
-//           engine: "text-embedding-ada-002",
-//         },
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             "api-key": API_KEY,
-//           },
-//         }
-//       );
-//       const embeddings = response.data.data[0].embedding;
-//       return embeddings;
-//     } catch (error) {
-//       console.error('Error during embeddings generation:', error);
-//       throw error;
-//     }
-//   };
-
-// }
-
-// export default SemanticSearchComponent;
