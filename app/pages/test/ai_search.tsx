3413c29e470c5477b782264db4d737b183d2694b
LAPTOP-5TUIN3RB\Z0192 <d-yamaguchi@link-tech.jp>
2024-01-17 23:26:07 +0900

    ・Menu Drawerの作成 ・Iconの追加   - IconクリックでPopoverを表示する(その中に条件のコントロールを配置)

--- app/pages/test/ai_search.tsx
+++ app/pages/test/ai_search.tsx
@@ -7,11 +7,82 @@ import { v4 as uuidv4 } from 'uuid';
 import { customLog } from '@/utils/customLog';
 
 
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
+//import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Tooltip from '@mui/material/Tooltip';
+import { styled, useTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MonitorIcon from '@mui/icons-material/Monitor';
+import HelpIcon from '@mui/icons-material/Help';
+import Popover from '@mui/material/Popover';
+
+// Material UI drawer sample code start
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+// Material UI drawer sample code end
 
 
 // ページコンポーネント
@@ -92,41 +163,138 @@ import MenuIcon from '@mui/icons-material/Menu';
     }   
   };
 
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const openPopover = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   // Pageコンポーネント
   return (
-    <div>
-<AppBar position="static">
-  <Toolbar variant="dense">
-    {/* <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-      <MenuIcon />
-    </IconButton> */}
-    <Typography variant="h6" color="inherit" component="div">
-      社内情報検索(Azure AI Search)
-    </Typography>
-  </Toolbar>
-</AppBar>
-      <div style={{ marginBottom: '-15px' }}>
-        <FormControlLabel control={<Checkbox defaultChecked  size="small"/>} label="AI Search結果をAzureOpenAIを使用して要約する" />
-      </div>
-      <div>
-        <FormControlLabel control={<Checkbox defaultChecked  size="small"/>} label="AI Search結果が存在しない場合、AzureOpenAIを使用して一般的な回答を取得する" />
-      </div>
-      <div style={{ position: "relative", height: "500px" }}>
-        <MainContainer>
-          <ChatContainer>
-            <MessageList>
-              {messages.map(msg => (
-                <Message key={msg.id} model={msg} />
-              ))}
-            </MessageList>
-            <MessageInput
-              placeholder="Type message here"
-              onSend={(value: string) => handleSendMessage(value)}
-            />
-          </ChatContainer>
-        </MainContainer>
-      </div>
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar variant="dense">
+        <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            社内情報検索(Azure AI Search)
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Tooltip title="Chat Setting">
+            <IconButton edge="end" color="inherit" aria-label="settings" onClick={handleClick} sx={{ marginRight: 0.5 }}>
+              <SettingsIcon />
+            </IconButton>
+          </Tooltip>
+          <Popover
+            id={id}
+            open={openPopover}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+          >
+            {/* <Typography sx={{ p: 2 }}>The content of the Popover.</Typography> */}
+            <Box style={{ marginBottom: '-15px' }}>
+              <FormControlLabel control={<Checkbox defaultChecked  size="small"/>} label="AI Search結果をAzureOpenAIを使用して要約する" />
+            </Box>
+            <Box>
+              <FormControlLabel control={<Checkbox defaultChecked  size="small"/>} label="AI Search結果が存在しない場合、AzureOpenAIを使用して一般的な回答を取得する" />
+            </Box>
+          </Popover>
+          <Tooltip title="Help">
+            <IconButton edge="end" color="inherit" aria-label="settings">
+              <HelpIcon />
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Home', 'AzureOpenAI', 'QA Search', 'Mail Checker'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                <MonitorIcon />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <Main open={open}>
+        <DrawerHeader />
+        {/* <Box style={{ marginBottom: '-15px' }}>
+          <FormControlLabel control={<Checkbox defaultChecked  size="small"/>} label="AI Search結果をAzureOpenAIを使用して要約する" />
+        </Box>
+        <Box>
+          <FormControlLabel control={<Checkbox defaultChecked  size="small"/>} label="AI Search結果が存在しない場合、AzureOpenAIを使用して一般的な回答を取得する" />
+        </Box> */}
+        <Box style={{ position: "relative", height: "500px" }}>
+          <MainContainer>
+            <ChatContainer>
+              <MessageList>
+                {messages.map(msg => (
+                  <Message key={msg.id} model={msg} />
+                ))}
+              </MessageList>
+              <MessageInput
+                placeholder="Type message here"
+                onSend={(value: string) => handleSendMessage(value)}
+              />
+            </ChatContainer>
+          </MainContainer>
+        </Box>
+      </Main>
+    </Box>
   );
 }
 export default ChatWithAiTest;
