bdcde9411c3739c9e75e13a77af8b327249b7fc8
LTPC065N\admin <d-yamaguchi@link-tech.jp>
2024-01-22 18:01:52 +0900

    QA-Searchの作成。http Requestが飛ぶところまで確認

--- app/pages/api/getCustomMessageFromQaSearch.ts
+++ app/pages/api/getCustomMessageFromQaSearch.ts
@@ -1,7 +1,13 @@
 import fetch from 'node-fetch';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { customLog } from '@/utils/customLog';
+import { AIRequest } from '@/utils/chat_gpt/ai_requset';
 
-const apiKey: string = 'YOUR_AZURE_API_KEY';
-const endpoint: string = 'YOUR_AZURE_QUESTION_ANSWERING_ENDPOINT';
+// const apiKey: string = 'YOUR_AZURE_API_KEY';
+// const endpoint: string = 'YOUR_AZURE_QUESTION_ANSWERING_ENDPOINT';
+
+const API_KEY: string = process.env.AZURE_API_KEY_QUESTION_ANSWERING!;
+const ENDPOINT: string = process.env.AZURE_ENDPOINT_QUESTION_ANSWERING!;
 
 interface RequestBody {
     question: string;
@@ -17,12 +23,12 @@ const requestOptions: RequestInit & { body: string } = {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `EndpointKey ${apiKey}`,
+        'Authorization': `EndpointKey ${API_KEY}`,
     },
     body: JSON.stringify(requestBody),
 };
 
-fetch(endpoint, requestOptions)
+fetch(ENDPOINT, requestOptions)
     .then(response => response.json())
     .then(data => {
         console.log('Answer:', data.answers);
@@ -30,3 +36,66 @@ fetch(endpoint, requestOptions)
     .catch(error => {
         console.error('Error:', error);
     });
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+if (req.method === 'POST') {
+    customLog(req.body,"DEBUG");
+    const requestMessage: string = req.body.message;
+    // const systemMessage: string = req.body.summarySystemMessage;
+    // req.body.summarySystemMessageが存在すればその値を、存在しなければデフォルト値を使用
+    const systemMessage: string = req.body.summarySystemMessage || "システムメッセージ";
+    
+    if(requestMessage == "") {
+    customLog("requestMessage is empty","DEBUG");
+    return res.status(200).json({ success: false, message: "request message is empty" });
+    }
+
+    // OpenAIのレスポンスのJSON型,適切な型リストが見つかり次第実装予定
+    let data: any;
+    const headers = new Headers();
+    headers.append("api-key", API_KEY);
+    headers.append("Content-Type", "application/json");
+    const ai_request = new AIRequest();
+    ai_request.model = "gpt-3.5-turbo";
+    ai_request.messages = [
+    // { role: "system", content: "システムメッセージ" },
+    { role: "system", content: systemMessage },
+    { role: "user", content: requestMessage }
+    //{ role: "assistant", content: "システムメッセージ" },
+    //{ role: "user", content: "システムメッセージ" },
+    ];
+
+    const body = ai_request.toJSON();
+    customLog(body,"DEBUG");
+
+    let requestOptions = {
+    method: "POST",
+    headers: headers,
+    body: body
+    };
+
+    let response: any = undefined;
+    try {
+    response = await fetch(AZURE_AI_SERVER_URL, requestOptions);
+    }catch(e){
+    console.log("Error : getCustomMessageFromChatGPT.ts is bad function");
+    console.log(e);
+    }
+    let responseMessage: string = "";
+    if (response.ok) {
+    data = await response.json();
+    responseMessage = data.choices[0].message.content;
+    if(responseMessage != ""){
+        customLog(data.choices[0].message,"DEBUG");
+        return res.status(200).json({ success: true, message: responseMessage });
+    } else {
+        customLog('response message is empty',"FUNCTION");
+    }
+    } else {
+    customLog('Network response was not ok',"FUNCTION");
+    }
+    return res.status(200).json({ success: false, message: "API response was not ok" });
+}
+};
+
--- app/pages/test/qa_search.tsx
+++ app/pages/test/qa_search.tsx
@@ -1,97 +1,22 @@
 import React, { useState } from 'react';
-import {FormControlLabel, Checkbox, Box, Typography} from '@mui/material';
-import { ExtendMessageModel } from '@/types'; // Add this import statement
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { ChatContainer, MainContainer, Message, MessageInput, MessageList, MessageModel } from '@chatscope/chat-ui-kit-react';
 import { v4 as uuidv4 } from 'uuid';
 import { customLog } from '@/utils/customLog';
 
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import SettingsIcon from '@mui/icons-material/Settings';
-import Tooltip from '@mui/material/Tooltip';
-import { styled, useTheme } from '@mui/material/styles';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MonitorIcon from '@mui/icons-material/Monitor';
-import HelpIcon from '@mui/icons-material/Help';
-import Popover from '@mui/material/Popover';
-import TextField from '@mui/material/TextField';
-
-// Material UI drawer sample code start
-const drawerWidth = 240;
-
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
-
-// Material UI drawer sample code end
-
-// HTMLタグを除去する関数
-function stripHtmlTags(input: string): string {
-  return input.replace(/<[^>]*>?/gm, '');
-}
-
 // ページコンポーネント
-const ChatWithAiTest = () => {
+function ChatWithAiTest() {
   // メッセージのstateを作成
-  const [messages, setMessages] = useState<Array<ExtendMessageModel>>([]);
+  const [messages, setMessages] = useState<Array<ExtendMessageModel>>([
+    {
+      id: uuidv4(),
+      message: "初期入力メッセージ",
+      sentTime: "just now",
+      sender: "Bot",
+      direction: "outgoing",
+      position: "single"
+    }
+  ]);
 
   // メッセージの送信機能の追加
   const handleSendMessage = async (messageText:string) => {
@@ -123,159 +48,57 @@ const ChatWithAiTest = () => {
     }
   };
 
-  const fetchData = async (context: string): Promise<string> => {
-    let data: any = undefined;
-    try{
-        const strippedContext = stripHtmlTags(context);
-        data = await fetch('/api/getCustomMessageFromAiSearch', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        // body: JSON.stringify({ message: context, useSummary, useFallback }),
-        //body: JSON.stringify({ message: context }),
-        body: JSON.stringify({ message: strippedContext  }),
-      });
-    }catch(e){
-      console.log("Error : chat_ai_test.tsx is bad function");
-      console.log(e);
-      return "API接続エラーです.";
-    }
-
-    if (data.ok) {
-      const response = await data.json();
-      if(response.success) {
-        // return response.message;
-        const resultMessage = response.message;
-        if (useSummary) {
-          try {
-            const additionalData = await fetch('/api/getCustomMessageFromChatGPT', {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              // body: JSON.stringify({ message: resultMessage }),
-              body: JSON.stringify({ message: resultMessage, summarySystemMessage}),
-            });
-
-            if (additionalData.ok) {
-              const additionalResponse = await additionalData.json();
-              if (additionalResponse.success) {
-                return additionalResponse.message;
-              } else {
-                customLog("Additional response message is empty" + additionalResponse.message);
-                return additionalResponse.message;
-              }
-            } else {
-              customLog("Additional response was failed");
-              return "エラーです";
-            }
-          } catch (additionalError) {
-            console.error('Error while fetching additional data:', additionalError);
-            return "エラーです";
-          }
-        }
-        return resultMessage;
-      } else {
-        customLog("response message is empty" + response.message);
-        return response.message;
-      }
-    } else {
-      customLog("response was failed");
-      return "エラーです";
-    }   
-  };
-
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
-
   // Pageコンポーネント
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar variant="dense">
-        <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            QA
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <Tooltip title="Help">
-            <IconButton edge="end" color="inherit" aria-label="settings">
-              <HelpIcon />
-            </IconButton>
-          </Tooltip>
-        </Toolbar>
-      </AppBar>
-
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {['Home', 'AzureOpenAI', 'AI Search', 'Mail Checker'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                <MonitorIcon />
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-
-      <Main open={open}>
-        <DrawerHeader />
-        <Box style={{ height: "calc(100vh - 120px)" }}>
-          <MainContainer>
-            <ChatContainer>
-              <MessageList>
-                {messages.map(msg => (
-                  <Message key={msg.id} model={msg} />
-                ))}
-              </MessageList>
-              <MessageInput
-                placeholder="Type message here"
-                onSend={(value: string) => handleSendMessage(value)}
-              />
-            </ChatContainer>
-          </MainContainer>
-        </Box>
-      </Main>
-    </Box>
+    <div>
+      <h1>Interaction with ChatGPT 3.5</h1>
+      <div style={{ position: "relative", height: "500px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map(msg => (
+                <Message key={msg.id} model={msg} />
+              ))}
+            </MessageList>
+            <MessageInput
+              placeholder="Type message here"
+              onSend={(value: string) => handleSendMessage(value)}
+            />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
   );
 }
+
+// APIを叩いてレスポンスを受ける.
+async function fetchData(context:string): Promise<string> {
+  let data: any = undefined;
+  try{
+    data = await fetch('/api/getCustomMessageFromQaSearch', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: context }),
+    });
+  }catch(e){
+    console.log("Error : chat_ai_test.tsx is bad function");
+    console.log(e);
+    return "API接続エラーです.";
+  }
+  if (data.ok) {
+    const response = await data.json();
+    if(response.success) {
+      return response.message;
+    } else {
+      customLog("response message is empty" + response.message);
+      return response.message;
+    }
+  } else {
+    customLog("response was failed");
+    return "エラーです";
+  }   
+}
+
 export default ChatWithAiTest;
 
 export interface ExtendMessageModel extends MessageModel {
